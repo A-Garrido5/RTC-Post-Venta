78d2b414abddbc509af1556d6fb6b6c1af272285
Rodrigo J.P <rjorquera@acsim.cl>
2015-10-23 11:12:44 -0300

    Correcciones en html y javascript

--- www/assets/js/index.js
+++ www/assets/js/index.js
@@ -2,60 +2,54 @@
 
 
 
-$(document).ready(function (){
+$(document).ready(function () {
 
-  //location.href="nivel1.html"
-
-	var value = window.localStorage.getItem("username");
-
-	
-
-	if (value===null){
+    var value = window.localStorage.getItem("username");
+    if (value === null) {
 
 
+        /*
+        var ubicacionActual= window.location.pathname;
 
-    var ubicacionActual= window.location.pathname;
+        //alert(ubicacionActual);
+        //if(ubicacionActual!="login.html"){
 
-    //alert(ubicacionActual);
-   //if(ubicacionActual!="login.html"){
+        if(ubicacionActual==="/data/data/com.adobe.phonegap.app/files/files/phonegapdevapp/www/index.html"){
 
-    if(ubicacionActual==="/data/data/com.adobe.phonegap.app/files/files/phonegapdevapp/www/index.html"){
+        }
 
-    }
+        else if(ubicacionActual==="/data/data/com.adobe.phonegap.app/files/files/phonegapdevapp/www/login.html"){
 
-    else if(ubicacionActual==="/data/data/com.adobe.phonegap.app/files/files/phonegapdevapp/www/login.html"){
+        }
 
-    }
+        else if(ubicacionActual==="/data/data/com.adobe.phonegap.app/files/files/phonegapdevapp/www/crearUsuario.html"){
 
-    else if(ubicacionActual==="/data/data/com.adobe.phonegap.app/files/files/phonegapdevapp/www/crearUsuario.html"){
+        }
 
-    }
-
-    else if(ubicacionActual!="/android_asset/www/login.html" && ubicacionActual!="/login.html" && ubicacionActual!="/crearUsuario.html" && ubicacionActual!="/android_asset/www/crearUsuario.html"){
+        else if(ubicacionActual!="/android_asset/www/login.html" && ubicacionActual!="/login.html" && ubicacionActual!="/crearUsuario.html" && ubicacionActual!="/android_asset/www/crearUsuario.html"){
         
         location.href="login.html";  
+        }
+
+        */
+        location.href = "login.html";
+        //javascript: mywindow.close(); ;
+        //$('#dialogLogin').show();
+        //$( "#dialogLogin" ).trigger( "click" );
+
+        //$( '#dialogLogin' ).click ();
+
+        //		document.getElementById('dialogLogin').onclick();
+
+
+
+
     }
+    else {
+        mostrarMenu();
 
-
-  
-		
-		//$('#dialogLogin').show();
-		//$( "#dialogLogin" ).trigger( "click" );
-
-		//$( '#dialogLogin' ).click ();
-
-//		document.getElementById('dialogLogin').onclick();
-
-		
-
-
-	}
-	else
-	{  
-    mostrarMenu();	
-		
-		$('#nameRight').text(value);
-	}
+        $('#nameRight').text(value);
+    }
 
 
 
@@ -63,54 +57,32 @@ $(document).ready(function (){
 });
 
 function redirigir(ruta){
-
 	location.href=ruta;
-
 }
 
 function logout(){
-
   localStorage.removeItem("username");
-
   location.href="login.html";
-
-
 }
 
+/*
 function login(datos){
 
-	
-	var urlLogin="http://sae1.imatronix.com:2614/WEBAPI_SERVICE/api/Login/"+datos; 
-	        
+	var urlLogin="http://sae1.imatronix.com:2614/WEBAPI_SERVICE/api/Login/"+datos;      
 	$.ajax({
           url: urlLogin,
           type: "GET",
           dataType: "json",
           success: function(json) {
-            //alert(JSON.stringify(json));
-
             var sesionvalida = parseInt(json.sesionValida);
             if(sesionvalida!=1){
               alert(json.mensaje);
                                   }
             else{
-             // alert("            "+json.mensaje +'\n'+json.nombres);
-
-				
-      				$('#nameRight').text(json.nombres);
-      				localStorage.setItem("username", json.nombres);
-
-              location.href="/android_asset/www/index.html";
-              
+      		  $('#nameRight').text(json.nombres);
+      		  localStorage.setItem("username", json.nombres);
+              location.href = "index.html";
             }
-                            
-            //document.getElementById('nombres').innerHTML=json.nombres.toString();
-            
-            //alert(json.nombres);
-               
-               
-      
-            
           },
           error:function (xhr, ajaxOptions, thrownError) {
              alert(JSON.stringify(thrownError));
@@ -119,37 +91,27 @@ function login(datos){
     });
 
 }
+*/
 
 function mostrarMenu(){
 
   var urlGetLevel ="http://sae1.imatronix.com:2614/WEBAPI_SERVICE/api/Nivel1";
-
   $.ajax({
           url: urlGetLevel,
           type: "GET",
           dataType: "json",
           success: function(json) {
-
-
             if (json != "") {
 
                var selectObject = $('#level1');
-
                var jsonObject = eval(json);
-
-                
-
                for (var n = 0; n < jsonObject.length; n++) {
-                    //selectObject.append(new Option(jsonObject[n].glosa, jsonObject[n].idRegion.value));
                     $('#level1').append($('<option>', { 
                         value: jsonObject[n].idNivel1,
                         text : jsonObject[n].glosa
                     }));
                };
             }
-
-               
-      
             
           },
           error:function (xhr, ajaxOptions, thrownError) {
@@ -164,44 +126,37 @@ function mostrarMenu(){
 
 function mostrarNivel2(idNivel1){
 
-
+    $("#menuDinamico").html("");
+    if (idNivel1 == 0) {
+        return;
+    }
   var urlGetLevel2 ="http://sae1.imatronix.com:2614/WEBAPI_SERVICE/api/Nivel2/"+idNivel1;
-
-
-  var divApertura = "<div class=\"row text-center\">\n";
-  var divCierre ="</div>\n";
+  var divApertura = "<div class='row text-center'>";
+  var divCierre ="</div>";
   var html = "";
   var html2 = "";
   var contenido = "";
-
-
-
+  
   $.ajax({
           url: urlGetLevel2,
           type: "GET",
           dataType: "json",
           success: function(json) {
 
-
-          
-              //alert(JSON.stringify(json));
-               
-
                 var jsonObject = eval(json);
-
                 html+=divApertura;
 
-                html+="  <div class='col-50'>\n";
-                html+="      <a href='about.html' class='menu-link' onclick='redirigir('ticket.html?idNivel1="+idNivel1+"&idNivel2="+json[0].idNivel2+")>\n";
-                html+="         <span class='"+json[0].imagen+"'></span>\n";
-                html+="         <span>"+json[0].glosa+"</span>\n"
+                html+="  <div class='col-50'>";
+                html+="      <a href='about.html' class='menu-link' onclick='redirigir('ticket.html?idNivel1="+idNivel1+"&idNivel2="+json[0].idNivel2+")>";
+                html+="         <span class='"+json[0].imagen+"'></span>";
+                html+="         <span>"+json[0].glosa+"</span>"
                 html+="      </a>"
                 html+="  </div>"; 
 
-                html+="  <div class='col-50'>\n";
-                html+="      <a href='about.html' class='menu-link' onclick='redirigir('ticket.html?idNivel1="+idNivel1+"&idNivel2="+json[1].idNivel2+")>\n";
-                html+="         <span class='"+json[1].imagen+"'></span>\n";
-                html+="         <span>"+json[1].glosa+"</span>\n"
+                html+="  <div class='col-50'>";
+                html+="      <a href='about.html' class='menu-link' onclick='redirigir('ticket.html?idNivel1="+idNivel1+"&idNivel2="+json[1].idNivel2+")>";
+                html+="         <span class='"+json[1].imagen+"'></span>";
+                html+="         <span>"+json[1].glosa+"</span>"
                 html+="      </a>"
                 html+="  </div>";  
 
@@ -209,18 +164,14 @@ function mostrarNivel2(idNivel1){
 
                 html2+=divApertura;
 
-                html2+="  <div class='col-50'>\n";
-                html2+="      <a href='about.html' class='menu-link' onclick='redirigir('ticket.html?idNivel1="+idNivel1+"&idNivel2="+json[2].idNivel2+")>\n";
-                html2+="         <span class='"+json[2].imagen+"'></span>\n";
-                html2+="         <span>"+json[2].glosa+"</span>\n"
+                html2+="  <div class='col-50'>";
+                html2+="      <a href='about.html' class='menu-link' onclick='redirigir('ticket.html?idNivel1="+idNivel1+"&idNivel2="+json[2].idNivel2+")>";
+                html2+="         <span class='"+json[2].imagen+"'></span>";
+                html2+="         <span>"+json[2].glosa+"</span>"
                 html2+="      </a>"
                 html2+="  </div>";
 
                 html2+=divCierre; 
-
-
-
-
                 
 
               /* for (var n = 0; n < jsonObject.length; n++) {              
@@ -267,13 +218,10 @@ function mostrarNivel2(idNivel1){
                     
                }*/
 
-               alert(html);
+               //alert(html);
 
                $("#menuDinamico").html(html + html2);
-            
-
-               
-      
+                
             
           },
           error:function (xhr, ajaxOptions, thrownError) {
@@ -301,6 +249,7 @@ function isNumber(e) {
       return patron.test(n);
 }
 
+/*
 $('#botonLogin').click(function() {
 
 
@@ -313,19 +262,11 @@ $('#botonLogin').click(function() {
 
 
 });
-
+*/
 
 $("#level1").change(function(){
-
   var valor = $("#level1").val();
-
-
-  mostrarNivel2(valor);
-  
-
-
-
-        
+  mostrarNivel2(valor);    
 });
 
 
--- www/assets/js/login.js
+++ www/assets/js/login.js
@@ -1,54 +1,53 @@
-function login(datos){
+    function login(datos){
+	    var urlLogin="http://sae1.imatronix.com:2614/WEBAPI_SERVICE/api/Login/"+datos;
 
-	
-	var urlLogin="http://sae1.imatronix.com:2614/WEBAPI_SERVICE/api/Login/"+datos; 
-	        
-	$.ajax({
-          url: urlLogin,
-          type: "GET",
-          dataType: "json",
-          success: function(json) {
-            //alert(JSON.stringify(json));
-            var sesionvalida = parseInt(json.sesionValida);
-            if(sesionvalida!=1){
-              alert(json.mensaje);
-                                  }
-            
-                            
-            //document.getElementById('nombres').innerHTML=json.nombres.toString();
-            
-            //alert(json.nombres);
-               
-               
-      
-            
-          },
-          error:function (xhr, ajaxOptions, thrownError) {
-             alert(JSON.stringify(thrownError));
-             alert(JSON.stringify(xhr));
-          }
+	    $.ajax({
+	        url: urlLogin,
+	        type: "GET",
+	        dataType: "json",
+	        success: function (json) {
+	            var sesionvalida = parseInt(json.sesionValida);
+	            if (sesionvalida != 1) {
+	                alert(json.mensaje);
+	            }
+	            else {
+	                $('#nameRight').text(json.nombres);
+	                localStorage.setItem("username", json.nombres);
+	                localStorage.setItem("token", json.token);
+	                location.href = "index.html";
+	            }
+	        },
+	        error: function (xhr, ajaxOptions, thrownError) {
+	            alert(JSON.stringify(xhr));
+	        }
+	    });
+
+    }
+
+    function isNumber(e) {
+          k = (document.all) ? e.keyCode : e.which;
+          if (k==8 || k==0) return true;
+          patron = /\w/ ;
+          n = String.fromCharCode(k);
+          return patron.test(n);
+    }
+
+    $('#botonLogin').click(function() { 
+            var datosUsuario = $("#nombredeusuario").val()
+            var datosPassword = $("#clave").val()
+            var cripto = window.btoa(datosUsuario+'|'+datosPassword);
+            login(cripto);
     });
 
-}
 
-function isNumber(e) {
-      k = (document.all) ? e.keyCode : e.which;
-      if (k==8 || k==0) return true;
-      patron = /\w/ ;
-      n = String.fromCharCode(k);
-      return patron.test(n);
-}
+    function crearCuenta() {
+        location.href = "crearUsuario.html";
+    }
 
-$('#botonLogin2').click(function() { 
-
-		alert("funciona boton");
-
-        var datosUsuario = $("#nombredeusuario").val()
-        var datosPassword = $("#clave").val()
-        var cripto = window.btoa(datosUsuario+'|'+datosPassword);
-
-        login(cripto);
-        
-
-
-});
+    function isNumber(e) {
+        k = (document.all) ? e.keyCode : e.which;
+        if (k == 8 || k == 0) return true;
+        patron = /\w/;
+        n = String.fromCharCode(k);
+        return patron.test(n);
+    }
